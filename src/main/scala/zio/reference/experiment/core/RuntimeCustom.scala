ebdbb402effedf574d15bb1a310463a4ea4a2e30
changzhiwin <changzhiwin@gmail.com>
2022-10-03 15:21:40 +0800

    understand use ZIOApp to override bootstrap

--- src/main/scala/zio/reference/experiment/core/RuntimeCustom.scala
+++ src/main/scala/zio/reference/experiment/core/RuntimeCustom.scala
@@ -1,79 +1,44 @@
 package zio.reference.experiment.core
 
+// 会初始化2遍: bootstrap/layer 分别一遍
+// 多次尝试后，还是没有弄明白，bootstrap里面的实例为啥没有提升到run的环境里面
+// Ask at stackoverflow
+// https://stackoverflow.com/questions/73915303/why-override-bootstrap-not-work-for-run-method/73919987#73919987
+
+// package example
+
 import zio._
-import zio.internal.stacktracer.Tracer
-
-trait LoggingService {
-  def log(line: String): UIO[Unit]
-}
-
-object LoggingService {
-  def log(line: String): URIO[LoggingService, Unit] =
-    ZIO.serviceWith[LoggingService](_.log(line))
-
-  val live: ZLayer[Any, Nothing, LoggingService] = ZLayer.succeed(LoggingServiceLive())
-}
-
 trait EmailService {
   def send(user: String, content: String): Task[Unit]
 }
-
 object EmailService {
   def send(user: String, content: String): ZIO[EmailService, Throwable, Unit] =
-    ZIO.serviceWith[EmailService](_.send(user, content))
+    ZIO.serviceWithZIO[EmailService](_.send(user, content))
 
-  val live: ZLayer[Any, Nothing, EmailService] = ZLayer.succeed(EmailServiceFake())
-}
-
-case class LoggingServiceLive() extends LoggingService {
-  override def log(line: String): UIO[Unit] =
-    Console.printLine(line).orDie
-    //ZIO.succeed(print(line))
+  val live: ZLayer[Any, Nothing, EmailService] = 
+    ZLayer.fromZIO( ZIO.succeed(EmailServiceFake()) <* Console.printLine("Init EmailService") ).orDie
 }
 
 case class EmailServiceFake() extends EmailService {
   override def send(user: String, content: String): Task[Unit] =
-    ZIO.log(s"sending email to $user")
-    //Console.printLine(s"sending email to $user")
-    //ZIO.attempt(println(s"sending email to $user"))
+    Console.printLine(s"sending email to $user")
 }
 
-object RuntimeCustom { // extends ZIOAppDefault {
+// Don't use ZIOAppDefault
+object RuntimeCustom extends ZIOApp {
 
-  //val layer = LoggingService.live ++ EmailService.live
+  override val bootstrap = EmailService.live
 
-  val runtime: Runtime[LoggingService with EmailService] =
-    Runtime.default.withEnvironment {
-    ZEnvironment[LoggingService, EmailService](LoggingServiceLive(), EmailServiceFake())
-  }
+  override type Environment = EmailService
 
-  def main(args: Array[String]): Unit = {
-    implicit val trace  = Tracer.newTrace
-    implicit val unsafe = Unsafe.unsafe
-    /*
-    Unsafe.unsafe { implicit unsafe =>
-        runtime.unsafe.run(
-          for {
-            f1 <- LoggingService.log("sending newsletter").fork
-            f2 <- EmailService.send("David", "Hi! Here is today's newsletter.").fork
-            _ <- f1.zip(f2).join
-            _ <- Console.printLine("Finished.")
-          } yield ()
-        ).getOrThrowFiberFailure()
-    }
-    */
-    runtime.unsafe.run {
-      (for {
-        runtime <- ZIO.runtime[LoggingService with EmailService]
-        _       <- runtime.run(run).tapErrorCause(ZIO.logErrorCause(_))
-      } yield())
-    }
-  }
-
-  def run = for {
-    _ <- LoggingService.log("I am LoggingService")
+  override val environmentTag: EnvironmentTag[Environment] = EnvironmentTag[Environment]
+  
+  def run = (for {
+    _ <- ZIO.debug("Start...")
     _ <- EmailService.send("God", "Hi")
-  } yield ()
+    _ <- ZIO.debug("End...")
+  } yield ())
 
-  //).provide(layer)
+  // This can works, have no doubt
+  //.provide(EmailService.live)
 }
